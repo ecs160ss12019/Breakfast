3018f9fdde6ee6eab2b926794963bc2ea1c2a066
Adila Abudureheman <aabudureheman@gmail.com>
2019-07-16 01:14:09 -0700

    Added Initial Skeleton classes for pellets

--- app/src/main/java/com/example/pacman/Pellet.java
+++ app/src/main/java/com/example/pacman/Pellet.java
@@ -4,6 +4,10 @@ package com.example.pacman;
 import android.graphics.Canvas;
 
 public interface Pellet {
-    public void draw(Canvas canvas);
-    public void updateStatus(long fps);
+    public int getX();
+    public int getY();
+    public int getType();
+    public int getPoint();
+
 }
+
--- app/src/main/java/com/example/pacman/PowerPellet.java
+++ app/src/main/java/com/example/pacman/PowerPellet.java
@@ -6,20 +6,26 @@ import android.graphics.Canvas;
 import android.util.Pair;
 
 public class PowerPellet implements Pellet{
+    private int type = 1;
+    private int mScreenX;
+    private int mScreenY;
+    private int point = 5;
 
     public PowerPellet(){
 
-
+    }
+    public int getX(){
+        return mScreenX;
+    }
+    public int getY(){
+        return mScreenY;
+    }
+    public int getType(){
+        return this.type;
+    }
+    public int getPoint(){
+        return point;
     }
 
-    @Override
-    public void draw(Canvas canvas) {
-
-    }
-
-    @Override
-    public void updateStatus(long fps) {
-
-    }
 
 }
