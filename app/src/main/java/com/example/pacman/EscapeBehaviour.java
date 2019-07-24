2bf64d85dde82b954e9c65f429ab404f7e6c86a0
Siqi Chai <sqchai@ucdavis.edu>
2019-07-24 07:01:59 -0700

    Abstracted ghost Behaviours that relate to escaping and die

---
+++ app/src/main/java/com/example/pacman/EscapeBehaviour.java
@@ -0,0 +1,12 @@
+package com.example.pacman;
+
+public class EscapeBehaviour implements GhostBehaviour {
+    @Override
+    public DirectionQueue performBehaviour() {
+        return null;
+    }
+
+    public EscapeBehaviour() {
+
+    }
+}
---
+++ app/src/main/java/com/example/pacman/KilledBehaviour.java
@@ -0,0 +1,12 @@
+package com.example.pacman;
+
+public class KilledBehaviour implements GhostBehaviour{
+    @Override
+    public DirectionQueue performBehaviour() {
+        return null;
+    }
+
+    public KilledBehaviour() {
+
+    }
+}
