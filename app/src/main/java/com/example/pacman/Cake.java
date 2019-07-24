ca9c40fb2bb6d9fc023a8ce0ff168ee7083f8656
Siqi Chai <32528321+sqchai@users.noreply.github.com>
2019-07-24 06:30:00 -0700

    Update Cake.java

--- app/src/main/java/com/example/pacman/Cake.java
+++ app/src/main/java/com/example/pacman/Cake.java
@@ -5,6 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
+import java.util.ArrayList;
+import java.util.Observer;
+import java.util.Random;
+
 /*
 * This is equivalent to the original cherry in Pac-Man game:
 * random generate the cake onto the sceen and double the points earned;
@@ -12,174 +16,93 @@ import android.graphics.Canvas;
 * the effect should disappear in few seconds;
 * the cake should move around.
 * */
-public class Cake extends Runner implements GameObject {
-
-    private Context context;
-    private boolean needToChangeDir =false;
-    private Bitmap rawCakeImg;
-    private Bitmap scaledCakeImg;
-
+public class Cake extends MovingObject implements GameObject {
     private boolean isDead;
     //eaten means whether points was added.
     private boolean eaten;
 
-    public boolean isDead() {
-        return isDead;
-    }
-
-    public void setDead(boolean dead) {
-        isDead = dead;
-    }
-
-    private Cake(Context context, TwoTuple screen, float speed, CollisionSubject collision) {
-        super(screen, speed, collision);
-        this.context = context;
-        currDirection = TwoTuple.LEFT;
-        nextDirection = -1;
-        rawCakeImg = BitmapFactory.decodeResource(context.getResources(), R.drawable.cake);
-        scaledCakeImg = Bitmap.createScaledBitmap(rawCakeImg,mScreen.y / 15, mScreen.y/15, true);
-        bitmapHeight = scaledCakeImg.getHeight();
-        bitmapWidth = scaledCakeImg.getWidth();
-    }
-
-    // Constructor1
-    public Cake(Context context, TwoTuple screen, float speed, CollisionSubject collision, Arcade arcade){
-        this(context, screen, speed, collision);
-
-        this.arcade = arcade;
-        this.posInScreen = arcade.getCakePosition_pix();
-        this.posInArcade = new TwoTuple(arcade.cakePosition);
-        this.posInArcadeInit = posInArcade;
-
-        motionInArcade = new MotionInArcade(arcade);
-    }
-
-    //Constructor2
-    public Cake(Context context, TwoTuple screen, float speed, CollisionSubject collision, Arcade arcade, ArcadeAnalyzer arcadeAnalyzer){
-        this(context, screen, speed, collision);
-
-        this.arcade = arcade;
-        this.posInArcade = new TwoTuple(arcade.cakePosition);
-        this.posInScreen = arcade.mapScreen(posInArcade);
-        this.posInArcadeInit = posInArcade;
-
-        this.arcadeAnalyzer = arcadeAnalyzer;
-        this.blockDimension = arcadeAnalyzer.blockDimension;
-    }
-
-    @Override
-    public void draw(Canvas canvas){
-        TwoTuple screenPos = arcade.mapScreen(posInArcade);
-        canvas.drawBitmap(scaledCakeImg, screenPos.first() - bitmapWidth/2,
-                        screenPos.second() - bitmapHeight/2, null);
-    }
-
-    /*
-    We use this func to calculate the after move location in a direction,
-    no matter the direction is valid or not.
-     */
-//    private TwoTuple move(int direction, long fps) {
-//        int nextX = this.xCoordiante;
-//        int nextY = this.yCoordinate;
+//    public boolean isDead() {
+//        return isDead;
+//    }
 //
-//        // Move the pacman based on the direction variable
-//        // and the speed of the previous frame
-//        if (direction == LEFT) {
-//            nextX = (int)(nextX - speed / fps);
-//        }
-//        if (direction == RIGHT) {
-//            nextX = (int)(nextX + speed / fps);
-//        }
-//        if (direction == UP) {
-//            nextY = (int)(nextY - speed / fps);
-//        }
-//        if (direction == DOWN) {
-//            nextY = (int)(nextY + speed / fps);
-//        }
-//
-//        // Stop the Pacman going off the screen
-//        if (nextX - bitmapWidth / 2 < 0) {
-//            nextX = bitmapWidth / 2;
-//        }
-//        if (nextX + bitmapWidth / 2 > mScreenX) {
-//            nextX = mScreenX - bitmapWidth / 2;
-//        }
-//        if (nextY - bitmapHeight / 2 < 0) {
-//            nextY = bitmapHeight / 2;
-//        }
-//        if (nextY + bitmapHeight / 2 > mScreenY) {
-//            nextY = mScreenY - bitmapHeight / 2;
-//        }
-//
-//        return new TwoTuple(nextX, nextY);
-//        canvas.drawBitmap(scaledCakeImg, position.x - bitmapWidth/2,
-//                        position.y - bitmapHeight/2, null);
+//    public void setDead(boolean dead) {
+//        isDead = dead;
 //    }
 
-    public void updateLocation(long fps, Arcade arcade) {
-        /*
-        We cannot update is the fps is -1,
-        otherwise there will be a overflow
-        in speed/fps.
-         */
-        if (fps == -1 || fps == 0) {
-            return;
-        }
+//    public void updateLocation(long fps, Arcade arcade) {
+//        /*
+//        We cannot update is the fps is -1,
+//        otherwise there will be a overflow
+//        in speed/fps.
+//         */
+//        if (fps == -1 || fps == 0) {
+//            return;
+//        }
+//
+//
+//        /***************************************/
+//        //New Method
+//        int mathematicalMove = mathematicalMoveDistance(fps);
+//
+//        if (nextDirection != currDirection && nextDirection != -1) {
+//            //try new direction
+//            boolean allowsTurn = arcadeAnalyzer.allowsToGo(posInArcade, nextDirection);
+//
+//            if (allowsTurn) {
+//                //Turn and go
+//                posInArcade = movedTo(mathematicalMove, nextDirection);
+//
+//                currDirection = nextDirection;
+//                needToChangeDir = false;
+//                return;
+//            }
+//        }
+//
+//        //Either not able to turn or not desired to turn
+//        boolean allowsMove = arcadeAnalyzer.allowsToGo(posInArcade, currDirection);
+//        if (allowsMove) {
+//            //move and go
+//            posInArcade = movedTo(mathematicalMove, currDirection);
+//            return;
+//        }
+//
+//        needToChangeDir = true;
+//
+//        //else no move, stay there
+//    }
+//
+//    //mathematical movement distance
+//    private int mathematicalMoveDistance(long fps) {
+//        return (int)(speed / fps);
+//    }
+//
+//    //move as far as possible
+//    private TwoTuple movedTo(int mathematicalMove, int movingDirection) {
+//        int movedDistance = 0;
+//        TwoTuple currPos = posInArcade;
+//
+////        System.out.println("Starting to move from: " + posInArcade.first() + " " + posInArcade.second());
+//        boolean allowsMove = arcadeAnalyzer.allowsToGo(currPos, movingDirection);
+//        while (movedDistance <= mathematicalMove && allowsMove) {
+//            movedDistance += arcadeAnalyzer.blockDimension;
+//            currPos = TwoTuple.moveTo(currPos, movingDirection);
+//
+////            System.out.println("moved distance: " + movedDistance);
+////            System.out.println("Moved to: " + currPos.first() + " " + currPos.second());
+//        }
+//
+////        System.out.println("!!!!!");
+////        System.out.println("Finished moving to: " + currPos.first() + " " + currPos.second());
+//        return currPos;
+//    }
 
-
-        /***************************************/
-        //New Method
-        int mathematicalMove = mathematicalMoveDistance(fps);
-
-        if (nextDirection != currDirection && nextDirection != -1) {
-            //try new direction
-            boolean allowsTurn = arcadeAnalyzer.allowsToGo(posInArcade, nextDirection);
-
-            if (allowsTurn) {
-                //Turn and go
-                posInArcade = movedTo(mathematicalMove, nextDirection);
-
-                currDirection = nextDirection;
-                needToChangeDir = false;
-                return;
-            }
-        }
-
-        //Either not able to turn or not desired to turn
-        boolean allowsMove = arcadeAnalyzer.allowsToGo(posInArcade, currDirection);
-        if (allowsMove) {
-            //move and go
-            posInArcade = movedTo(mathematicalMove, currDirection);
-            return;
-        }
-
-        needToChangeDir = true;
-
-        //else no move, stay there
+    public void setNextDirection() {
+        Random random = new Random();
+        this.motionInfo.nextDirection = random.nextInt(4);
     }
 
-    //mathematical movement distance
-    private int mathematicalMoveDistance(long fps) {
-        return (int)(speed / fps);
-    }
-
-    //move as far as possible
-    private TwoTuple movedTo(int mathematicalMove, int movingDirection) {
-        int movedDistance = 0;
-        TwoTuple currPos = posInArcade;
-
-//        System.out.println("Starting to move from: " + posInArcade.first() + " " + posInArcade.second());
-        boolean allowsMove = arcadeAnalyzer.allowsToGo(currPos, movingDirection);
-        while (movedDistance <= mathematicalMove && allowsMove) {
-            movedDistance += arcadeAnalyzer.blockDimension;
-            currPos = TwoTuple.moveTo(currPos, movingDirection);
-
-//            System.out.println("moved distance: " + movedDistance);
-//            System.out.println("Moved to: " + currPos.first() + " " + currPos.second());
-        }
-
-//        System.out.println("!!!!!");
-//        System.out.println("Finished moving to: " + currPos.first() + " " + currPos.second());
-        return currPos;
+    //Constructor
+    public Cake(final MotionInfo motionInfo, final ArrayList<Bitmap> viewList){
+        super(motionInfo, viewList);
     }
 }
