e96de772958167bfb66d609b52fd6ca852355dc6
Siqi Chai <32528321+sqchai@users.noreply.github.com>
2019-07-24 06:29:31 -0700

    Update BitmapDivider.java

--- app/src/main/java/com/example/pacman/BitmapDivider.java
+++ app/src/main/java/com/example/pacman/BitmapDivider.java
@@ -1,42 +1,48 @@
 package com.example.pacman;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import java.util.ArrayList;
 
 /*
 This class helps to split collection of bitmap
-into a list of small bitmaps. Each src file for
+into a list of small views. Each src file for
 a game character is stored as a collection of the
 character facing each direction. Thus, cutting the
 img apart is necessary.
  */
 public class BitmapDivider {
-    private ArrayList<Bitmap> bitmaps;
-    private Bitmap bitmap;
-    private int originalFileWidth;
-    private int originalFileHeight;
-    private int cuttingWidth;
-    private int cuttingHeight;
+    private Context context;
 
-    public ArrayList<Bitmap> split(int numRow, int numCol) {
-        cuttingWidth = (int)(originalFileWidth/numCol);
-        cuttingHeight = (int)(originalFileHeight/numRow);
+    public static ArrayList<Bitmap> splitAndResize(Bitmap bitmap,
+                                            TwoTuple dimension, TwoTuple targetDimension) {
+        ArrayList<Bitmap> views = new ArrayList<>();
+
+        int numRow = dimension.x;
+        int numCol = dimension.y;
+        int cuttingWidth = (int)(bitmap.getWidth()/numCol);
+        int cuttingHeight = (int)(bitmap.getHeight()/numRow);
 
         //cutting start
         for(int i = 0; i < numRow; i++) {
             for (int j = 0; j < numCol; j++) {
                 Bitmap next = Bitmap.createBitmap(bitmap, j * cuttingWidth, i * cuttingHeight,
                         cuttingWidth, cuttingHeight);
-                bitmaps.add(next);
+
+                next = Bitmap.createScaledBitmap(next, targetDimension.x, targetDimension.y, true);
+                views.add(next);
             }
         }
-        return bitmaps;
+        return views;
     }
 
-    public BitmapDivider(Bitmap bitmap) {
-        this.bitmap = bitmap;
-        bitmaps = new ArrayList<>();
-        originalFileWidth = bitmap.getWidth();
-        originalFileHeight = bitmap.getHeight();
+    public Bitmap loadBitmap(final int resId) {
+        return BitmapFactory.decodeResource(context.getResources(), resId);
+    }
+
+    public BitmapDivider(final Context context) {
+        this.context = context;
     }
 }
