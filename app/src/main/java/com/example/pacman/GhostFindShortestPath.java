58974aad7e8c39f2cb3043cc6bfa44615d449560
Hivenwu <hivenwu1995@gmail.com>
2019-07-24 14:59:42 -0700

    pseudocode of find the shorest path (incomplete)

--- app/src/main/java/com/example/pacman/GhostFindShortestPath.java
+++ app/src/main/java/com/example/pacman/GhostFindShortestPath.java
@@ -1,7 +1,26 @@
 package com.example.pacman;
 
+import java.lang.*;
+
 
 
 public class GhostFindShortestPath {
-    //
+
+    public GhostFindShortestPath(){
+        //get the valid direction by calling "FindTheValidDirrection"
+        //check each direction's Distance to the pacman
+
+    }
+
+
+    // Find the Valid direction at the current position
+    public void FindTheValidDirrection (){
+        //get the current position of ghost
+        //check all four direction
+        // store the valid direcrion
+    }
+
+    public void FindDistancetoPacman(){
+        //sqare root of (Vertical absolute sqare + Horizontal absolute square)
+    }
 }
