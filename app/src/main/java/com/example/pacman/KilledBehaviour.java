7bf1f3ddeee026266ca1da6f412cfbeaa75c7b32
Hivenwu <hivenwu1995@gmail.com>
2019-07-27 04:09:02 -0700

    Updated killed behaviour

--- app/src/main/java/com/example/pacman/KilledBehaviour.java
+++ app/src/main/java/com/example/pacman/KilledBehaviour.java
@@ -3,7 +3,11 @@ package com.example.pacman;
 public class KilledBehaviour implements GhostBehaviour{
     @Override
     public int performBehaviour(MotionInfo ghostMotion, MotionInfo pacmanMotion, MotionInfo reference, ArcadeAnalyzer arcadeAnalyzer) {
-        return 0;
+        MotionInfo motionInfo = new MotionInfo();
+        motionInfo.posInArcade = new TwoTuple(17, 17);
+
+        ChaseBehaviour chaseBehaviour = new ChaseBehaviour();
+        return chaseBehaviour.performBehaviour(ghostMotion, motionInfo, null, arcadeAnalyzer);
     }
 
     public KilledBehaviour() {
