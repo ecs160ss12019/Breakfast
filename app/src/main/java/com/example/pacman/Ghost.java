1b4e5e4e93611aea7d8de159d2672ec038d0c815
Alessandro <alle95liu@gmail.com>
2019-07-28 14:41:43 -0700

    Merge branch 'master' of /Users/alessandro/AndroidStudioProjects/Breakfast with conflicts.

--- app/src/main/java/com/example/pacman/Ghost.java
+++ app/src/main/java/com/example/pacman/Ghost.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.Color;
 
 import java.util.ArrayList;
 import java.util.Observer;
