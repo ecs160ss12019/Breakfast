a6a5bdac4ba47355ca9c6c05b5aa8dc80df6bb25
Hivenwu <hivenwu1995@gmail.com>
2019-07-28 18:59:53 -0700

    Ghost behavior change in Ghost. instead of GOC

--- app/src/main/java/com/example/pacman/Ghost.java
+++ app/src/main/java/com/example/pacman/Ghost.java
@@ -1,20 +1,159 @@
 package com.example.pacman;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.graphics.Color;
 
 import java.util.ArrayList;
-import java.util.Observer;
-import java.util.Queue;
-import java.util.Random;
+
+/*
+id: 0 red   chase
+    1 pink  chaseFront
+    2 blue  chasePredict
+    3 yellow    chaseSearch
+ */
 
 public class Ghost extends MovingObject {
     public int id;
     public GhostBehaviour ghostBehaviour;
     public GameObjectTimer gameObjectTimer;
+    private static TwoTuple door = new TwoTuple(11,13);
+    private static TwoTuple RebornPos = new TwoTuple(14,14);
+    private ArrayList<GhostBehaviour> chaseBehaviourList;
+    private ArrayList<GhostBehaviour> scatterBehaviourList;
+
+    public void updateGhostBehavior(boolean powerPelletEaten, boolean collision) {
+        final GhostBehaviour currBehaviour = this.ghostBehaviour;
+        final boolean isChasing = this.chaseBehaviourList.contains(currBehaviour);
+        final boolean isScatter = this.scatterBehaviourList.contains(currBehaviour);
+        final boolean timeUp = gameObjectTimer.timeUp;
+        final TwoTuple currPos = this.motionInfo.posInArcade;
+
+        if (currBehaviour instanceof GhostStationaryBehaviour && timeUp) {
+
+            this.ghostBehaviour = new GhostExitBehaviour();
+            return;
+        }
+
+        if (currBehaviour instanceof GhostExitBehaviour && currPos == door) {
+
+            switch (id) {
+                case 0:
+                    this.ghostBehaviour = new GhostChaseBehaviour();
+                    break;
+                case 1:
+                    this.ghostBehaviour = new GhostChaseFrontBehaviour();
+                    break;
+                case 2:
+                    this.ghostBehaviour = new GhostPredictAndChaseBehaviour();
+                    break;
+                case 3:
+                    this.ghostBehaviour = new GhostSearchAndChaseBehaviour();
+                    break;
+            }
+
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.chaseTime);
+            return;
+        }
+
+        if (isChasing && powerPelletEaten) {
+            this.ghostBehaviour = new GhostEscapeBehaviour();
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.powerUp);
+            return;
+        }
+
+        if (isChasing && timeUp) {
+
+            switch (id) {
+                case 0:
+                    this.ghostBehaviour = new GhostScatterLeftTop();
+                    break;
+                case 1:
+                    this.ghostBehaviour = new GhostScatterRightTop();
+                    break;
+                case 2:
+                    this.ghostBehaviour = new GhostScatterLeftBottom();
+                    break;
+                case 3:
+                    this.ghostBehaviour = new GhostScatterRightBottom();
+                    break;
+            }
+
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.scatterTime);
+            return;
+        }
+
+        if (isScatter && powerPelletEaten) {
+            this.ghostBehaviour = new GhostEscapeBehaviour();
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.powerUp);
+            return;
+        }
+
+        if (isScatter && timeUp) {
+
+            switch (id) {
+                case 0:
+                    this.ghostBehaviour = new GhostChaseBehaviour();
+                    break;
+                case 1:
+                    this.ghostBehaviour = new GhostChaseFrontBehaviour();
+                    break;
+                case 2:
+                    this.ghostBehaviour = new GhostPredictAndChaseBehaviour();
+                    break;
+                case 3:
+                    this.ghostBehaviour = new GhostSearchAndChaseBehaviour();
+                    break;
+            }
+
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.chaseTime);
+            return;
+        }
+
+        if (currBehaviour instanceof GhostEscapeBehaviour && timeUp) {
+
+            switch (id) {
+                case 0:
+                    this.ghostBehaviour = new GhostChaseBehaviour();
+                    break;
+                case 1:
+                    this.ghostBehaviour = new GhostChaseFrontBehaviour();
+                    break;
+                case 2:
+                    this.ghostBehaviour = new GhostPredictAndChaseBehaviour();
+                    break;
+                case 3:
+                    this.ghostBehaviour = new GhostSearchAndChaseBehaviour();
+                    break;
+            }
+
+            this.gameObjectTimer = new GameObjectTimer(GameObjectTimer.chaseTime);
+            return;
+        }
+
+        if (currBehaviour instanceof GhostEscapeBehaviour && collision) {
+            this.ghostBehaviour = new GhostKilledBehaviour();
+
+            return;
+        }
+
+        if(currBehaviour instanceof GhostKilledBehaviour && currPos == door){
+            this.ghostBehaviour = new GhostEnterBehaviour();
+
+            return;
+        }
+
+        if(currBehaviour instanceof GhostEnterBehaviour && currPos == RebornPos){
+
+            this.ghostBehaviour = new GhostExitBehaviour();
+
+            return;
+        }
+
+
+
+
+
+
+    }
 
     //Constructor
     public Ghost(final int id, final MotionInfo motionInfo, final ArrayList<Bitmap> viewList,
@@ -25,5 +164,17 @@ public class Ghost extends MovingObject {
         this.ghostBehaviour = ghostBehaviour;
 
         this.gameObjectTimer = new GameObjectTimer(CountDownTime);
+
+        this.chaseBehaviourList = new ArrayList<>();
+        chaseBehaviourList.add(new GhostChaseBehaviour());
+        chaseBehaviourList.add(new GhostChaseFrontBehaviour());
+        chaseBehaviourList.add(new GhostPredictAndChaseBehaviour());
+        chaseBehaviourList.add(new GhostSearchAndChaseBehaviour());
+
+        this.scatterBehaviourList = new ArrayList<>();
+        scatterBehaviourList.add(new GhostScatterLeftTop());
+        scatterBehaviourList.add(new GhostScatterRightTop());
+        scatterBehaviourList.add(new GhostScatterLeftBottom());
+        scatterBehaviourList.add(new GhostScatterRightBottom());
     }
 }
