332f3eadfe149722891325972c5ca18fd89bb537
Hivenwu <hivenwu1995@gmail.com>
2019-07-28 19:00:51 -0700

    Ghost Exit behavior

---
+++ app/src/main/java/com/example/pacman/GhostExitBehaviour.java
@@ -0,0 +1,8 @@
+package com.example.pacman;
+
+public class GhostExitBehaviour implements GhostBehaviour {
+    @Override
+    public int performBehaviour(MotionInfo ghostMotion, MotionInfo pacmanMotion, MotionInfo reference, ArcadeAnalyzer arcadeAnalyzer) {
+        return 0;
+    }
+}
